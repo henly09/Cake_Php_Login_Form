b23ae692e4c1bbb8ddcddda9eb56a52b95d61b54
John Henly A. Montera <monterahens@gmail.com>
2023-07-30 20:04:06 +0800

    Small Revisions on Login

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -4,24 +4,28 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-
-<div >
     <div class="row justify-content-center align-items-center" style="height: 100vh">
         <!-- in /templates/Users/login.php -->
-        <div class="col-12 col-md-6 order-md-2">
+        <div class="col-8 col-md-5 order-md-2">
             <div class="users form content">
                 <?= $this->Flash->render() ?>
                 <div class="col-12 text-center">
                     <h3 style="font-size: 30px; font-weight: bold;">Login</h3>
                 </div>
                 <?= $this->Form->create() ?>
-                <fieldset>
-                    <?= $this->Form->control('email', ['required' => true]) ?>
-                    <?= $this->Form->control('password', ['required' => true]) ?>
-                </fieldset>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-11">
+                        <fieldset class="mx-auto">
+                            <?= $this->Form->control('email', ['required' => true]) ?>
+                            <?= $this->Form->control('password', ['required' => true]) ?>
+                        </fieldset>
+                        </div>
+                    </div>
+                    </div>
                 <div class="d-flex justify-content-center">
                     <?= $this->Html->link(__('Signup'), ['controller' => 'Users', 'action' => 'signup'], ['class' => 'button mr-3']) ?>
-                    <?= $this->Form->submit(__('Login'), ['class' => 'btn btn-primary']) ?>
+                    <?= $this->Form->submit(__('Login'), ['class' => 'btn', 'style' => 'background-color: #66afdf;']) ?>
                 </div>
                 <?= $this->Form->end() ?>
             </div>
@@ -31,4 +35,3 @@
             <!-- You can add any content you want here -->
         </div>
     </div>
-</div>
